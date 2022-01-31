2fd5dd01025286b476deb2952f2ef46415df7919
Tomer Figenblat <tomer.figenblat@gmail.com>
2022-01-31 17:24:28 +0200

    chore: added license header verification for the full archetypes

--- java-full-mod/src/main/resources/archetype-resources/src/main/java/AppInterface.java
+++ java-full-mod/src/main/resources/archetype-resources/src/main/java/AppInterface.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright ${year} ${ownerName}.
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
 package $package;
 
 /** Application interface used for contracting applications. */
--- java-full-mod/src/test/resources/projects/prepare-deployment-mod-it/reference/src/it/simple-it/src/main/java/archetypes/it/prepare/deployment/mod/simple/it/AppImplementation.java
+++ java-full-mod/src/test/resources/projects/prepare-deployment-mod-it/reference/src/it/simple-it/src/main/java/archetypes/it/prepare/deployment/mod/simple/it/AppImplementation.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2042 Jon Doe.
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
 package archetypes.it.prepare.deployment.mod.simple.it;
 
 import archetypes.it.prepare.deployment.mod.AppInterface;
