ae297fab7b91c1e77f5c2f32267abe3f2e460191
TomerFi <TomerFi@users.noreply.github.com>
2022-12-09 12:32:06 +0000

    docs: deployed to gh-pages for refs/tags/1.1.9

--- generator.js
+++ generator.js
@@ -3,7 +3,7 @@ const PATTERN_ARTIFACT = /^.*[a-zA-Z0-9]+.*$/;
 const PATTERN_VERSION = /^(\d+)\.(\d+)\.(\d+)(?:-SNAPSHOT)?$/;
 const PATTERN_YEAR = /^\d{4}$/;
 
-const currentVersion = "1.1.7"
+const currentVersion = "1.1.9"
 
 $(document).ready(function() {
     function showProps(className, isModular) {
